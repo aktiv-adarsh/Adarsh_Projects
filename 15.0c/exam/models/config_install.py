8034d92a19ed9b001450502ed5dad600bc4a1d7b
aktiv-adarsh <adarsh.d@icreativetechnolabs.com>
2022-04-06 15:30:00 +0530

    Exam model

--- 15.0c/exam/models/config_install.py
+++ 15.0c/exam/models/config_install.py
@@ -9,17 +9,28 @@ class ConfigInstall(models.TransientModel):
 
     today_date = fields.Date.today()
     module_hr = fields.Boolean(string="Employees")
+    print("\n------------Month-[", today_date.month, "]---------\n")
     current_month_order_ids = fields.Many2many('sale.order', domain=[('date_order', 'like', 'today_date.month')])
 
+
+
+    #
     # @api.model
     # def get_values(self):
     #     res = super(ConfigInstall, self).get_values()
-    #     env_rec = self.env['ir.config_parameter'].get_param('exam.current_month_order_ids', self.current_month_order_ids)
-    #     res.update({
-    #     "current_month_order_ids": env_rec.order_line})
+    #     print("--------Get-Val--", res, "----------")
+    #     for rec in self.current_month_order_ids:
+    #         res.update({"order_line": self.env['ir.config_parameter'][(0, 0, {'product_id': rec.id})]})
+    #         print("\n--------Get_Val-For---", res, "------------\n")
+    #
     #     return res
     #
     # def set_values(self):
     #     res = super(ConfigInstall, self).set_values()
     #     self.env['ir.config_parameter'].set_param('exam.current_month_order_ids', self.current_month_order_ids)
+    #     print("\n---------Set-Val-[", res, "]------------\n")
     #     return res
+
+
+
+
--- 15.0c/exam/wizards/wizard_model.py
+++ 15.0c/exam/wizards/wizard_model.py
@@ -4,11 +4,21 @@ from odoo import models, fields, api
 
 
 class ExamWizard(models.TransientModel):
-
     _name = 'exam.wizard'
     _description = 'exam.wizard'
 
-    m2o_product_id = fields.Many2many('product.template', string="Product Many2One1")
+    m2o_product_id = fields.Many2many('product.template', string="Product Many2One1", required=True)
+    product_qnty = fields.Float(string="Quantity")
+    product_price = fields.Float(string="Unit Price")
 
-    def Wizard1(self):
-        print("------------Save data-------", self)
+    def so_button_action(self):
+        record = self.env['res.partner'].browse(self.env.context.get('active_id').partner_id.id)
+        # record = self.env['res.partner'].search([('active_id', '=', self.env.context.get('active_id'))])
+        print("\n-----------------", record, "------------\n")
+        # for rec in self.m2o_product_id:
+        #     print("-------------In For loop---------------")
+        #     record.write({'order_line': [(0, 0, {'product_id': rec.id})]})
+        #     print("-------------", rec, "---------------")
+
+        # def Wizard1(self):
+        #     print("------------Save data-------", self)
