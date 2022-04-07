51c0276c72204dbc841f73e61f604ccc9e48a65f
aktiv-adarsh <adarsh.d@icreativetechnolabs.com>
2022-04-07 19:02:23 +0530

    Update Exam model

--- 15.0c/exam/models/config_install.py
+++ 15.0c/exam/models/config_install.py
@@ -1,5 +1,6 @@
 
 from odoo import models, fields, api, _
+from datetime import *
 
 
 class ConfigInstall(models.TransientModel):
@@ -7,30 +8,27 @@ class ConfigInstall(models.TransientModel):
     """Here setting is inherited on TransientModel"""
     _inherit = 'res.config.settings'
 
-    today_date = fields.Date.today()
+    # today_date = fields.Date.today()
+    today_date = date.today()
     module_hr = fields.Boolean(string="Employees")
     print("\n------------Month-[", today_date.month, "]---------\n")
-    current_month_order_ids = fields.Many2many('sale.order', domain=[('date_order', 'like', 'today_date.month')])
+    current_month_order_ids = fields.Many2many('sale.order', domain=[('date_order', '>=',
+                                               datetime(datetime.today().year, datetime.today().month, 1)),
+                                              ('date_order', '<',
+                                               datetime(datetime.today().year, datetime.today().month+1, 1))])
 
+    @api.model
+    def get_values(self):
+        res = super(ConfigInstall, self).get_values()
+        env_data = self.env['ir.config_parameter'].get_param('exam.current_month_order_ids')
+        print("\n---------env_data--", env_data, "----------", type(env_data))
+        if env_data:
+            res.update(current_month_order_ids=[(6, 0, eval(env_data))])
+            print("\n--------Get_Val-For---", res, "------------\n")
 
+        return res
 
-    #
-    # @api.model
-    # def get_values(self):
-    #     res = super(ConfigInstall, self).get_values()
-    #     print("--------Get-Val--", res, "----------")
-    #     for rec in self.current_month_order_ids:
-    #         res.update({"order_line": self.env['ir.config_parameter'][(0, 0, {'product_id': rec.id})]})
-    #         print("\n--------Get_Val-For---", res, "------------\n")
-    #
-    #     return res
-    #
-    # def set_values(self):
-    #     res = super(ConfigInstall, self).set_values()
-    #     self.env['ir.config_parameter'].set_param('exam.current_month_order_ids', self.current_month_order_ids)
-    #     print("\n---------Set-Val-[", res, "]------------\n")
-    #     return res
-
-
-
-
+    def set_values(self):
+        self.env['ir.config_parameter'].set_param('exam.current_month_order_ids', self.current_month_order_ids.ids)
+        res = super(ConfigInstall, self).set_values()
+        print("\n---------Set-Val-[", res, "]------------\n", self.current_month_order_ids, self.current_month_order_ids.ids)
--- 15.0c/exam/wizards/wizard_model.py
+++ 15.0c/exam/wizards/wizard_model.py
@@ -7,14 +7,14 @@ class ExamWizard(models.TransientModel):
     _name = 'exam.wizard'
     _description = 'exam.wizard'
 
-    m2o_product_id = fields.Many2many('product.template', string="Product Many2One1", required=True)
+    m2o_product_id = fields.Many2one('product.template', string="Product Many2One1", required=True)
     product_qnty = fields.Float(string="Quantity")
     product_price = fields.Float(string="Unit Price")
 
     def so_button_action(self):
-        record = self.env['res.partner'].browse(self.env.context.get('active_id').partner_id.id)
+        # record = self.env['res.partner'].browse(self.env.context.get('active_id').partner_id.id)
         # record = self.env['res.partner'].search([('active_id', '=', self.env.context.get('active_id'))])
-        print("\n-----------------", record, "------------\n")
+        print("\n-----------------", self._context.get('context_records'), "------------\n")
         # for rec in self.m2o_product_id:
         #     print("-------------In For loop---------------")
         #     record.write({'order_line': [(0, 0, {'product_id': rec.id})]})
