5d4e11ae3971a9d8a1054fb97dceea93c59a733b
aktiv-adarsh <adarsh.d@icreativetechnolabs.com>
2022-05-17 18:50:12 +0530

    Employee contracts

--- 15.0c/empl_contracts/models/empl_contracts.py
+++ 15.0c/empl_contracts/models/empl_contracts.py
@@ -5,19 +5,20 @@ from odoo.addons.portal.controllers import portal
 
 class EmplContracts(http.Controller):
 
-    @http.route('/emp_contracts', type='http', auth='user', website=True)
+    @http.route('/my/emp_contracts', type='http', auth='user', website=True)
     def contracts_data(self, **kw):
         """This function will get the employees records"""
 
-        res = request.env['hr.employee'].sudo().search([])
+        res = request.env['hr.contract'].sudo().search([('employee_id', '=', request.env.user.employee_id.id)])
+        # print("\n\n\n----------------emp_id", res.employee_id.id)
+
         # print("\n\n------FUNC--in empl_contracts-", res, "------\n\n\n")
         return request.render('empl_contracts.emp_contracts_list_view', {'contract': res})
 
-    @http.route(['/emp_contracts/<model("hr.contract"):record>'], type='http', auth='public', website=True)
-    def link2_contract_details(self, record):
-
-        print("\n\n>>>>>>>>>--RECORDS ARE ->->->[", record, "]----\n")
-        return request.render('empl_contracts.emp_contracts_link_2details', {'link_record': record})
+    @http.route(['/my/emp_contracts/<model("hr.contract"):record>'], type='http', auth='user', website=True)
+    def link_to_contract_details(self, record):
+        # print("\n\n\n\n Function Executed \n\n\n")
+        return request.render("empl_contracts.contracts_link_to_details", {'records': record})
 
 
 class ContractsCounts(portal.CustomerPortal):
@@ -26,7 +27,7 @@ class ContractsCounts(portal.CustomerPortal):
         """To display all records total at my account"""
 
         values = super()._prepare_home_portal_values(counters)
-        counted = request.env['hr.contract'].search_count([])
+        counted = request.env['hr.contract'].search_count([('employee_id', '=', request.env.user.employee_id.id)])
         # print("\n\n--------counted-", counted, "\n\n")
         values.update({'contracts_count': counted})
         return values
